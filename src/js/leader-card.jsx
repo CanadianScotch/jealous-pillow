7faa0771fc4cd402ff2daf2f6629abb34f22a883
Matthew Ruxton <158092732+CanadianScotch@users.noreply.github.com>
2025-07-11 18:28:29 -0400

    Updated by drag-n-drop

---
+++ src/js/leader-card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LeaderCard = () => {
+  return (
+    <div>
+      <h3>School Leader Assignment</h3>
+      <p>This is a verified, working CRM card.</p>
+    </div>
+  );
+};
+
+export default LeaderCard;
